5f0f0d91a9c7d1545bad957ee3106afc26315f9c
N. V. Lang <git@nvlang.dev>
2025-03-26 15:14:07 +0100

    docs(egal): add module documentation
    The `@module egal` part can be useful for JSR (see
    https://jsr.io/docs/writing-docs#module-documentation).

--- packages/egal/src/mod.ts
+++ packages/egal/src/mod.ts
@@ -1,2 +1,24 @@
+/* eslint-disable tsdoc/syntax */
+/**
+ * @packageDocumentation
+ * The `egal` module exports a function `egal` that can be used to generate
+ * colors which are perceptually uniform with respect to both lightness and
+ * chroma (saturation).
+ *
+ * @example
+ * ```ts
+ * import { egal } from 'egal';
+ * console.log(egal(50, 100, 0));
+ * console.log(egal(50, 100, 100));
+ * ```
+ *
+ * This will output two colors in the `oklch` color space (one with hue 0 and
+ * one with hue 100), which have the same perceived lightness and saturation,
+ * and where, if the chroma were increased at all, some hues would not be able
+ * to "keep up" with the chroma increase at the same lightness.
+ *
+ * @module egal
+ */
+
 export { egal } from './egal.js';
 export type { EgalOptions, OutputColor, OutputFormat } from './types.js';
