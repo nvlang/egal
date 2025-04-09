a0eed4def2dfe940e27386439fc71631141fe352
N. V. Lang <git@nvlang.dev>
2025-04-09 02:13:43 +0200

    test(postcss): clean up e2e test

--- packages/postcss-egal/tests/e2e/sveltekit-tailwindcss/e2e/color.test.ts
+++ packages/postcss-egal/tests/e2e/sveltekit-tailwindcss/e2e/color.test.ts
@@ -1,10 +1,18 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1 with expected color', async ({ page }) => {
+test('home page has expected headers with expected colors', async ({
+    page,
+}) => {
     await page.goto('/');
     await expect(page.locator('h1')).toBeVisible();
     await expect(page.locator('h1')).toHaveCSS(
         'color',
+        'oklch(0.8 0.10012 175 / 0.5)',
+    );
+    await expect(page.locator('h1')).toHaveCSS(
+        'border-color',
         'oklch(0.8 0.10012 175)',
     );
+    await expect(page.locator('h2')).toBeVisible();
+    await expect(page.locator('h2')).toHaveCSS('color', 'oklch(0.6 0.10201 0)');
 });
--- packages/postcss-egal/tests/e2e/sveltekit-tailwindcss/src/routes/+page.svelte
+++ packages/postcss-egal/tests/e2e/sveltekit-tailwindcss/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <h1>Test 1</h1>
-<h2 class="text-testcolor-0-400">Test 2</h2>
+<h2 class="text-testcolor-400">Test 2</h2>
 
 <style>
     h1 {
-        color: egal(80% 100% 175);
+        color: egal(80% 100% 175 / 0.5);
         border: 1px solid egal(80% 100% 175);
     }
 </style>
